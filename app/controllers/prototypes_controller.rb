80a71c1c89b98fb7e8ccefb3fc10dac02d6aa412
konpeisan <introjection.19@icloud.com>
2021-05-22 20:03:04 +0900

    ユーザーのアクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -24,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
   end
 
   def update
